58790426ea33f8863444d42dfd6a155a62a712f6
Bruno Pena <brunoph26@gmail.com>
2023-03-23 23:41:58 -0500

    Adding form partial on new/edit views and method set_article on controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -23,11 +23,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -37,8 +35,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :photo)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 end

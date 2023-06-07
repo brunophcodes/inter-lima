6cf01c0bea2ca27c1a3f56efd4d4806a6e114e9a
Bruno Pena <brunoph26@gmail.com>
2023-06-06 22:18:24 -0500

    Adding carousel to homepage

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+import { register } from "swiper/element/bundle";
 
 const application = Application.start()
+register();
 
 // Configure Stimulus development experience
 application.debug = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "swiper/element/bundle", to: "https://ga.jspm.io/npm:swiper@9.3.2/element/swiper-element-bundle.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
+pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@9.3.2/swiper-bundle.esm.js"

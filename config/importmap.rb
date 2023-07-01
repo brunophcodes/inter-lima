ed52287df6d6bbf9308e01f1aaa8c9886de41738
Bruno Pena <brunoph26@gmail.com>
2023-06-30 23:57:29 -0500

    adding stimulus components

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,12 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "swiper/element/bundle", to: "https://ga.jspm.io/npm:swiper@9.3.2/element/swiper-element-bundle.js"
 pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
 pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@9.3.2/swiper-bundle.esm.js"
+pin "stimulus-scroll-progress", to: "https://ga.jspm.io/npm:stimulus-scroll-progress@4.1.0/dist/stimulus-scroll-progress.mjs"
+pin "lodash", to: "https://ga.jspm.io/npm:lodash@4.17.21/lodash.js"
+pin "stimulus-scroll-to", to: "https://ga.jspm.io/npm:stimulus-scroll-to@4.1.0/dist/stimulus-scroll-to.mjs"

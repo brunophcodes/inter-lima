f974a77738a008b58933d8dca3320ef46915a2e7
Bruno <brunoph26@gmail.com>
2023-11-09 21:57:03 -0500

    Updating creators cards and images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal:true
+
+# Manage the different pages of the website
 class PagesController < ApplicationController
   def home
     @banner_articles = Article.all.reverse
   end
 
-  def about 
-  end
+  def about; end
 end

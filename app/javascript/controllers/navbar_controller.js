a8ac2654475f9a8558187ebca7db226f3a85f895
Bruno <brunoph26@gmail.com>
2023-12-06 17:14:26 -0500

    updating navbar stimulus controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -46,13 +46,3 @@ export default class extends Controller {
   }
 
 }
-
-
-
-
-
-// https://betterprogramming.pub/show-and-hide-elements-with-rails-7s-stimulus-e988c35fbfb3 
-// https://stimulus.hotwired.dev/reference/targets 
-// https://www.storyly.io/post/interactive-content-vs-static-content 
-// https://stackoverflow.com/questions/38616990/how-do-static-variables-behave-between-multiple-targets 
-// https://discourse.cmake.org/t/multiple-executables-thus-multiple-different-builds-of-the-same-static-lib/8081 

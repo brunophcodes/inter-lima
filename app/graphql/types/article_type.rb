0fc8f4811fc451e0a762fb533e80bcb17df26e7e
Bruno Peña <brunoph26@gmail.com>
2025-12-28 21:21:53 -0500

    feat: add graphql config for articles photo

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -7,5 +7,14 @@ module Types
     field :body, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :photo_url, String, null:true 
+
+    def photo_url 
+      if object.photo.attached?
+        Rails.application.routes.url_helpers.polymorphic_url(object.photo, only_path: true)
+      end
+    rescue
+      nil
+    end
   end
 end
